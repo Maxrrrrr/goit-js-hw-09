18d7e5fbb274b5e464ae9c0854badd489aaa6d7d
Maxrrrrr <pavliv.maxym@gmail.com>
2023-10-15 23:40:06 +0300

    zxzX

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,6 @@
-asdasdas
+function makeTransaction (quantity, pricePerDroid){
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
+}
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500));
