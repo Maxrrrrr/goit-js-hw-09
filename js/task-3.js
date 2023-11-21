cec7bb31bc4623687da15439800b4b5768731b96
Maxrrrrr <pavliv.maxym@gmail.com>
2023-11-22 00:08:09 +0200

    1212

--- js/task-3.js
+++ js/task-3.js
@@ -1,27 +1,17 @@
 function filterArray(numbers, value) {
-  let filteredNumbers = [];
+  const filteredArray = [];
 
-  for (const i = 0; i < numbers.length; i++) {
-      if (numbers[i] > value) {
-          filteredNumbers.push(numbers[i]);
-      }
+  for (let i = 0; i < numbers.length; i++) {
+  
+    if (numbers[i] > value) {
+      filteredArray.push(numbers[i]);
+    }
   }
 
-  return filteredNumbers;
+  return filteredArray;
 }
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
-
-let randomArray = [];
-for (const j = 0; j < 5; j++) {
-  randomArray.push(Math.floor(Math.random() * 50));
-}
-let randomValue = Math.floor(Math.random() * 50);
-
-console.log("Випадковий масив: " + randomArray);
-console.log("Випадкове число: " + randomValue);
-console.log("Результат: " + filterArray(randomArray, randomValue));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
