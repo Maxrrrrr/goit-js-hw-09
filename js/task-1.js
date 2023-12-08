9da71e88b393d5fffa8c474a1655177801b6c3fd
Maxrrrrr <pavliv.maxym@gmail.com>
2023-12-08 22:56:38 +0200

    task 4

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,14 @@
-function slugify(title) {
-     let lowerCaseTitle = title.toLowerCase();
-     
-     let slug = lowerCaseTitle.split(" ");
-     
-     return slug.join("-");
- }
- 
+function isEnoughCapacity(products, containerSize) {
+    const totalProducts = Object.values(products).reduce((acc, curr) => acc + curr, 0);
+    return totalProducts <= containerSize;
+  }
 
- console.log(slugify("Arrays for begginers"));
- console.log(slugify("English for developer"));
- console.log(slugify("Ten secrets of JavaScript"));
- console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+  console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); 
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); 
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); 
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); 
